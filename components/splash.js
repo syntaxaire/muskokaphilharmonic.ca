68760a1c689b699b8d90f4970ec6bbbc03dbeb68
syntaxaire <syntaxaire@gmail.com>
2023-10-28 12:46:36 -0400

    Update with posters

--- components/splash.js
+++ components/splash.js
@@ -14,7 +14,7 @@ export const Splash = () => {
           </p>
           <a
             href='https://www.eventbrite.com/e/muskoka-philharmonic-orchestra-in-concert-winters-awakening-tickets-742979560867?aff=oddtdtcreator'
-            className='block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+            className='py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
             target='_blank'
           >
             <button
@@ -24,11 +24,23 @@ export const Splash = () => {
               Get tickets
             </button>
           </a>
+          <a
+            href='/events/2023-11-26/Blue Festive MPO Nov Poster - digital.pdf'
+            className='py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+            target='_blank'
+          >
+            <button
+              type='button'
+              className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+            >
+              See full poster (PDF)
+            </button>
+          </a>
         </div>
         <Image
           className='mt-8'
           src='/events/2023-11-26/winters_awakening.jpg'
-          alt="Winter's Awakening concert poster for November 26, 2023"
+          alt='Concert announcement for November 26, 2023'
           width='940'
           height='470'
         />
