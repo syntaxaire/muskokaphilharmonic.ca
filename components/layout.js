e0e73c905fb96e7a561290f6fb6fa8736c6b031a
syntaxaire <syntaxaire@gmail.com>
2023-10-28 11:43:09 -0400

    Set DM Serif as default

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,7 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 const Layout = ({ children }) => {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       {children}
     </main>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { DM_Serif_Display } from 'next/font/google';
+import { DM_Serif_Text } from 'next/font/google';
+
+export const dmserif = DM_Serif_Text({
+  subsets: ['latin'],
+  weight: '400',
+});
+export const dmserifdisp = DM_Serif_Display({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={dmserif.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
