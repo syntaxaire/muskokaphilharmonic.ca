27b79956a81ad94b964a72751115add811c41a14
syntaxaire <syntaxaire@gmail.com>
2023-12-01 17:43:40 -0500

    Remove old 'about' component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,70 @@
+import { FullWidthContainer } from '@/components/layout/containers';
+import { AboutSplash } from '@/components/splash';
+
 const { default: Layout } = require('@/components/layout/layout');
 
 const About = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <FullWidthContainer className="bg-white text-mpo-bg-dark">
+        <AboutSplash />
+        <section className="bg-white">
+          <div className="py-8 px-4 mx-auto max-w-screen-lg lg:py-16 lg:px-6">
+            <div className="max-w-screen-lg sm:text-lg">
+              <p className="mb-4 font-light">
+                The Muskoka Philharmonic Orchestra is based in Muskoka, Ontario.
+              </p>
+              <p className="mb-4 text-normal">
+                We perform a wide variety of music, and we are looking for new
+                members. We rehearse in Huntsville, and perform each year in
+                various locations around Muskoka.
+              </p>
+              <br />
+              <h4>Board of Directors</h4>
+              Co-Chair: Nancy Gibson
+              <br />
+              Co-Chair: Michael Gibson
+              <br />
+              Artistic Director: Alana Neudling
+              <br />
+              Event Planner/Manager: Jennifer Brockett
+              <br />
+              Secretary: Vaughn Adamson
+              <br />
+              Treasurer: Gerri Mar
+              <br />
+              <br />
+              <br />
+              <h4>Artistic Director</h4>
+              <p className="mb-4 text-normal">
+                Artistic Director, Alana Nuedling, grew up in the Muskoka area
+                and has been active in the music and arts community from an
+                early age. She has a Performance Diploma (Cambrian College), an
+                Honours BA in Music Education (Wilfrid Laurier University), and
+                a Bachelor of Education (Nipissing University).
+              </p>
+              <p>
+                Alana has been a member of the Orillia Silver Band, Muskoka
+                Concert Band, Sudbury Symphony Orchestra, Spirit of the Lakes
+                Drum and Bugle Corps, and many Salvation Army brass bands
+                throughout the years.
+              </p>
+              <p>
+                As a Music Director, Alana has led choirs, bands, pit
+                orchestras, and now is the Artistic Director/Co-Founder of the
+                newly formed Muskoka Philharmonic Orchestra.
+              </p>
+              <p>
+                Alana is thrilled to be working with a passionate group of
+                musicians who share the same love of collaboration, growth, and
+                performance through collective effervescence.
+              </p>
+            </div>
+          </div>
+        </section>
+      </FullWidthContainer>
+    </Layout>
+  );
 };
 
 export default About;
