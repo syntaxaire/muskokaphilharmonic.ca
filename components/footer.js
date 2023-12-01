ab2989f385f0d9fc3d2e88f578b38666a19ba4a5
syntaxaire <syntaxaire@gmail.com>
2023-12-01 17:44:07 -0500

    Add social links

--- components/footer.js
+++ components/footer.js
@@ -5,27 +5,26 @@ const Footer = () => {
   return (
     <footer className="bg-white rounded-lg m-4">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center">
+        <span className="text-sm text-mpo-semidark sm:text-center">
           © 2023 Muskoka Philharmonic Orchestra. All Rights Reserved.
         </span>
         <span className="flex">
-          <Link href="https://facebook.com/">
+          <Link href="https://www.facebook.com/profile.php?id=61552773276806">
             <FacebookIcon />
           </Link>
-          <Link href="https://instagram.com/">
+          <Link href="https://www.instagram.com/muskokaphilharmonic/">
             <InstagramIcon />
           </Link>
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-mpo-semidark sm:mt-0">
           <li>
-            You can contact us at{' '}
+            You can contact us at:{' '}
             <a
               href="mailto:huntsvillemusicians@gmail.com"
               className="hover:underline"
             >
               huntsvillemusicians@gmail.com
             </a>
-            .
           </li>
         </ul>
       </div>
