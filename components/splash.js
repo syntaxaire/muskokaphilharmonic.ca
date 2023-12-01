4cdd004ded57a0f1d50696ad1a76478f70af1360
syntaxaire <syntaxaire@gmail.com>
2023-12-01 17:44:51 -0500

    Add About splash

--- components/splash.js
+++ components/splash.js
@@ -1,4 +1,4 @@
-export const Splash = () => {
+export const HomeSplash = () => {
   return (
     <section className="min-h-[800px] bg-[url('/events/2023-11-26/concert_photo.jpg')] bg-no-repeat bg-cover bg-center bg-gray-400 bg-blend-multiply">
       <div className="relative py-8 px-4 mx-auto max-w-screen-xl text-white lg:py-16 z-1">
@@ -8,6 +8,23 @@ export const Splash = () => {
             live orchestral music
             <br />
             to Muskoka!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export const AboutSplash = () => {
+  return (
+    <section className="min-h-[600px] bg-[url('/about_splash.jpg')] bg-no-repeat bg-cover bg-center bg-gray-400 bg-blend-multiply">
+      <div className="relative py-8 px-4 mx-auto max-w-screen-xl text-white lg:py-16 z-1">
+        <div className="mb-6 lg:mb-0 text-right tracking-tight">
+          <p className="[text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)] mt-[300px] mr-[50px] mb-4 text-4xl md:text-5xl lg:text-5xl bg-mpo-bg-dark">
+            For the <br />
+            love of
+            <br />
+            <span className="text-mpo-accent">music</span>!
           </p>
         </div>
       </div>
