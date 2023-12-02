735c4b4d88fa944670b74408f74cc4e4bf7052bc
syntaxaire <syntaxaire@gmail.com>
2023-12-01 19:39:20 -0500

    We should improve lint somewhat

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     'eslint:recommended',
     'next/core-web-vitals',
     'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
     'prettier',
   ],
   plugins: ['jsx-a11y'],
@@ -13,5 +14,14 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], //should add ".ts" if typescript project
     // suppress errors for unvalidated props
     'react/prop-types': 'off',
+    // detect violations of React Rules of Hooks
+    'react-hooks/rules-of-hooks': 'error',
+    // warn about exhaustive dependencies for useEffect and similar hooks
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
