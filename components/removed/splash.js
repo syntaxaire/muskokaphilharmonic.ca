574ac719dce6e17ea2cb6956817d665c356d961b
Jeff Robertson <syntaxaire@gmail.com>
2024-02-19 15:02:11 -0500

    Fix broken image

--- components/removed/splash.js
+++ components/removed/splash.js
@@ -17,7 +17,7 @@ export const HomeSplash = () => {
 
 export const AboutSplash = () => {
   return (
-    <section className="min-h-[600px] bg-[url('/about_splash.jpg')] bg-no-repeat bg-cover bg-center bg-gray-400 bg-blend-multiply">
+    <section className="min-h-[600px] bg-[url('/images/about_splash.jpg')] bg-no-repeat bg-cover bg-center bg-gray-400 bg-blend-multiply">
       <div className="relative py-8 px-4 mx-auto max-w-screen-xl text-white lg:py-16 z-1">
         <div className="mb-6 lg:mb-0 text-right tracking-tight">
           <p className="[text-shadow:_2px_2px_0_rgb(0_0_0_/_40%)] mt-[300px] mr-[50px] mb-4 text-4xl md:text-5xl lg:text-5xl">
